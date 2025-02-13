92edafbe580753848d2353e911cfbf54edca7c48
victoormga <vvvvvggggaaaa11@gmail.com>
2025-02-13 21:49:25 +0100

    Added some changes and the backend.

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+//Main route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+//Start server
+app.listen(PORT, () => {
+    console.log(`Servidor en ejecución en http://localhost:${PORT}`);
+});
