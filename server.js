5687a9662cc7c72575f4dec440fa2b9f8f6354e5
victoormga <vvvvvggggaaaa11@gmail.com>
2025-02-19 22:10:54 +0100

    Added port 8080, failed.

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-//Serve static files
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-//Main route
+// Main route
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-//Start server
+// Start server on both ports
 app.listen(PORT, () => {
     console.log(`Servidor en ejecución en http://localhost:${PORT}`);
 });
